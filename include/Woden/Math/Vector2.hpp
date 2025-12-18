232c79595f82e0098177e2f7f6f3b366459e44e7
Ronie Salgado <ronie@desromech.cl>
2025-12-18 16:58:22 -0300

    I am starting to add the math library.

---
+++ include/Woden/Math/Vector2.hpp
@@ -0,0 +1,17 @@
+#ifndef WODEN_MATH_VECTOR2_HPP
+#define WODEN_MATH_VECTOR2_HPP
+
+#include "Scalar.hpp"
+
+namespace Woden
+{
+namespace Math
+{
+/**
+ * 2D vector.
+ */
+
+}    
+}
+
+#endif // WODEN_MATH_VECTOR2_HPP
---
+++ tests/Math/Vector2.cpp
@@ -0,0 +1,6 @@
+#include "UnitTest++/UnitTest++.h"
+#include "Woden/Math/Vector2.hpp"
+
+TEST(Vector2)
+{
+}
